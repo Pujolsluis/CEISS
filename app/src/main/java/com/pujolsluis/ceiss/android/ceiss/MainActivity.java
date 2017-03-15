2a054b964ce0570cae963e55e9d7e32320c9df6c
Luis Pujols <luispujols@hotmail.com>
2017-03-14 23:41:19 -0400

    ADD 'Created the subscribe button for the welcome screen, and now users can subscribe to the student committee newsletter'

--- app/src/main/java/com/pujolsluis/ceiss/android/ceiss/MainActivity.java
+++ app/src/main/java/com/pujolsluis/ceiss/android/ceiss/MainActivity.java
@@ -1,7 +1,11 @@
 package com.pujolsluis.ceiss.android.ceiss;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button subscribeBtn = (Button) findViewById(R.id.subscribe_buttonView);
+
+        subscribeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://eepurl.com/csDFZr";
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
